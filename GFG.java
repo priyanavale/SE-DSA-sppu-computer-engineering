6c19a3ca198344c20022761a77e28b6c107d5835
priyanavale <153231302+priyanavale@users.noreply.github.com>
2024-03-31 23:31:29 +0530

    GFG.java

---
+++ GFG.java
@@ -0,0 +1,28 @@
+// JAVA program to count number of even 
+import java.io.*; 
+
+class GFG { 
+
+	static void CountingEvenOdd(int arr[], int arr_size) 
+	{ 
+		int even_count = 0; 
+		int odd_count = 0;  
+		for (int i = 0; i < arr_size; i++) { 
+			if ((arr[i] & 1) == 1) 
+				odd_count++; 
+			else
+				even_count++; 
+		} 
+
+		System.out.println("Number of even"
+						+ " elements = " + even_count 
+						+ " Number of odd elements = "
+						+ odd_count); 
+	} 
+	public static void main(String[] args) 
+	{ 
+		int arr[] = { 2, 3, 4, 5, 6 }; 
+		int n = arr.length; 
+		CountingEvenOdd(arr, n); 
+	} 
+} 
